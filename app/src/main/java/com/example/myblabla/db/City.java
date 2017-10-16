954310e1b010dedcce04d8c310e91056ebbb883c
Zhaofei Yin <zhaofei.yin@gmail.com>
2017-10-16 17:06:00 +0200

    Add city and province

---
+++ app/src/main/java/com/example/myblabla/db/City.java
@@ -0,0 +1,44 @@
+package com.example.myblabla;
+
+/**
+ * Created by yijia_gong on 16/10/17.
+ */
+
+public class City {
+    private int id;
+    private String cityName;
+    private int cityCode;
+    private int provinceId;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+}
---
+++ app/src/main/java/com/example/myblabla/db/Province.java
@@ -0,0 +1,37 @@
+package com.example.myblabla;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by yijia_gong on 16/10/17.
+ */
+
+public class Province extends DataSupport{
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+}
